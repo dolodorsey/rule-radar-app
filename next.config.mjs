992dadb5e9b8ea73adf48283a7caa2024f9d1bf8
dolodorsey <dolodorsey@gmail.com>
2026-06-03 17:58:58 -0400

    feat: enable static export for Capacitor [launch]

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
